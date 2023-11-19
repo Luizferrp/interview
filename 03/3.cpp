22bb727f7fea19747118576ec8712b47f28f74d8
Luizferrp <luizreis.p@icloud.com>
2023-11-19 20:57:41 -0300

    adding module operations

--- 03/3.cpp
+++ 03/3.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-int m3, m5;
+int m3, m5, i;
 int main (int argc, char *argv[]) {
-  for (int i = 0; i <= 100; i++){
+  for (; i <= 100; i++){
+    m3 = (i % 3 == 0)? 0 : 1; m5 = (i % 5 == 0)? 0 : 1;
     std::cout << ((m3 == 0) ? ((m5 == 0) ? "FooBar" : "Foo") : ((m5 == 0) ? ("Bar") : std::to_string(i).c_str())) << "\n";
   }
   return 0;
