9861890ff8d494dad332d745922df0a1b605d045
Luizferrp <luizreis.p@icloud.com>
2023-11-19 20:54:38 -0300

    fixing types on output

--- 03/3.cpp
+++ 03/3.cpp
@@ -2,7 +2,7 @@
 int m3, m5;
 int main (int argc, char *argv[]) {
   for (int i = 0; i <= 100; i++){
-    std::cout << ((m3 == 0) ? ((m5 == 0) ? "FooBar" : "Foo") : ((m5 == 0) ? ("Bar") : i)) << "\n";
+    std::cout << ((m3 == 0) ? ((m5 == 0) ? "FooBar" : "Foo") : ((m5 == 0) ? ("Bar") : std::to_string(i).c_str())) << "\n";
   }
   return 0;
 }
