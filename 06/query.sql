746ca63083e79a7b58e74f284b81726d81d1f9da
Luizferrp <luizreis.p@icloud.com>
2023-11-19 22:07:57 -0300

    functional base of query

---
+++ 06/query.sql
@@ -0,0 +1,6 @@
+use questao6;
+SELECT Students.id,
+  CASE WHEN Notes.Grade <= 7 THEN NULL ELSE Students.name END AS name, Notes.Grade
+  FROM Students
+  INNER JOIN Notes ON Notes.Max_Value > Students.value AND Notes.Min_Value < Students.value;
+
