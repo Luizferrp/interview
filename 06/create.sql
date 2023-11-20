233d1b7fd0ab08bf2fe919387f4fde83bee4f170
Luizferrp <luizreis.p@icloud.com>
2023-11-19 22:06:52 -0300

    inserting examples for the query

--- 06/create.sql
+++ 06/create.sql
@@ -12,3 +12,24 @@ CREATE TABLE IF NOT EXISTS Notes (
   Max_Value INT,
   PRIMARY KEY(Grade)
 );
+INSERT INTO Notes ( Grade, Min_Value, Max_Value )
+VALUES
+  (1, 0, 9 ),
+  (2, 10, 19),
+  (3, 20, 29),
+  (4, 30, 39),
+  (5, 40, 49),
+  (6, 50, 59),
+  (7, 60, 69),
+  (8, 70, 79),
+  (9, 80, 89),
+  (10, 90, 100);
+
+INSERT INTO Students ( id, name, value )
+VALUES
+  ( 1, 'Julia', 81 ),
+  ( 2, 'Carol', 68 ),
+  ( 3,'Maria', 99 ),
+  ( 4,'Andreia', 78),
+  ( 5,'jaqueline', 63),
+  ( 6,'Marcela', 88);
