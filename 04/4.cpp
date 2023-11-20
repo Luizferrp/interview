492faf755bc548318c5e9d09f1d8d1b8521464ac
Luizferrp <luizreis.p@icloud.com>
2023-11-19 23:50:56 -0300

    fixing cases of q4

--- 04/4.cpp
+++ 04/4.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
-#include <string>
-
-int ConcatRemove(std::string base, std::string objetivo, int movimentos){
-  int i;
-  while (base[i] == objetivo[i]){
-    i++;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+string ConcatRemove(string& s, string& t, int k){
+  int n = s.size(), m = t.size();
+  int lo = 0, up = min(n, m), mid = (up+lo)>>1;
+  while(lo < mid){
+    if(s[mid] == t[mid]) lo = mid;
+    else up = mid;
+    mid = (up+lo)>>1;
   }
-  return ((objetivo.length() - i) + (base.length() - i));
+  return (n + m - 2*up <= k and ((n+m)%2 == k%2 or (n+m <= k))) ? "yes": "no";
 }
 
 int main (int argc, char *argv[]) {
@@ -15,6 +18,6 @@ int main (int argc, char *argv[]) {
   std::getline(std::cin, base);
   std::getline(std::cin, objetivo);
   std::cin >> mov;
-  std::cout << (mov >= ConcatRemove(base, objetivo, mov) ? "yes" : "no") << std::endl;
+  std::cout << ConcatRemove(base, objetivo, mov) << std::endl;
   return 0;
 }
